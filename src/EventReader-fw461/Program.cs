9d3a5becaef50aa047b1d3ff2200df4e83109cac
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-11-23 08:25:57 +0000

    implemented multiple options for .461sender

---
+++ src/EventReader-fw461/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using EventStore.ClientAPI;
+
+namespace EventReader_fw461
+{
+    partial class Program
+    {
+        private static IEventStoreConnection _conn;
+        private static ConnectionFactory _connectionFactory;
+
+        static void Main(string[] args)
+        {
+            var port = 1113;
+            if (args.Length > 0 && int.TryParse(args[0], out port))
+                Console.WriteLine($"Connecting to localhost on port {port}");
+            
+            try
+            {
+                _connectionFactory = new ConnectionFactory(port);
+                _connectionFactory.ConnectionClosed += ConnFactory_ConnectionClosed;
+                _conn = _connectionFactory.GetOpenConnection().Result;
+                do
+                {
+                    Console.WriteLine("Insert a stream name and press Enter to read events from (Press ctrl+C to exit");
+                    var stream = Console.ReadLine();
+                    ReadEvents(stream, _conn);
+                } while (true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void ConnFactory_ConnectionClosed(object sender, EventArgs e)
+        {
+            _conn = _connectionFactory.GetOpenConnection().Result;
+        }
+        
+        private static void ReadEvents(string stream, IEventStoreConnection conn)
+        {
+            try
+            {
+                var results = conn.ReadStreamEventsForwardAsync(stream, StreamPosition.Start, 10, false).GetAwaiter().GetResult();
+                foreach (var evt in results.Events)
+                    Console.WriteLine(Encoding.UTF8.GetString(evt.Event.Data));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        
+    }
+}
--- src/EventSender-fw461/Program.cs
+++ src/EventSender-fw461/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using EventStore.ClientAPI;
+using EventStore.ClientAPI.Projections;
+using EventStore.ClientAPI.SystemData;
+using Newtonsoft.Json;
 
 namespace ConsoleApp3
 {
@@ -8,24 +14,43 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            var port = 1113;
-            if (args.Length > 0 && int.TryParse(args[0], out port))
-                Console.WriteLine($"Connecting to localhost on port {port}");
-            var conn = EventStoreConnection.Create(GetConnectionBuilder(), new Uri($"tcp://localhost:{port}"));
+            var conn = EventStoreConnection.Create(GetConnectionBuilder(), new Uri($"tcp://localhost:1113"));
+            var projectionsManager = new ProjectionsManager(new TestLogger(), new IPEndPoint(IPAddress.Loopback, 2113),
+                TimeSpan.FromSeconds(5));
+            var projectionName = "LockeshTest";
+            var streamName = "ciccio1";
             try
             {
                 conn.Reconnecting += Conn_Reconnecting;
                 conn.ConnectAsync().Wait();
-                ConsoleKeyInfo key;
                 do
                 {
-                    Console.WriteLine("Press A to publish test event (any other key to leave)");
-                    key = Console.ReadKey();
+                    Console.Clear();
+                    Console.WriteLine($"Press P to create projection {projectionName}");
+                    Console.WriteLine($"Press S to send 1000 events to stream {streamName}");
+                    Console.WriteLine($"Press O to send 1 event to stream {streamName}");
+                    Console.WriteLine($"Press D to  Disable projection '{projectionName}'");
+                    Console.WriteLine($"Press E to  Enable projection '{projectionName}'");
+                    Console.WriteLine($"Press A to set $maxAge=2 to stream {streamName}");
+                    Console.WriteLine($"Press C to set $maxCount=2 to stream {streamName}");
+                    var key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.O || key.Key == ConsoleKey.NumPad0)
+                        AppendToStreamAsync(conn, streamName, 1);
+                    if (key.Key == ConsoleKey.S)
+                        AppendToStreamAsync(conn, streamName, 1000);
+                    if (key.Key == ConsoleKey.P)
+                        projectionsManager.CreateContinuousAsync(projectionName,
+                            $"fromStream('{streamName}').when({{'$any': function(state, evnt) {{linkTo('OurTargetStreamName', evnt);  }}}});",
+                            new UserCredentials("admin", "changeit"));
+                    if (key.Key == ConsoleKey.D)
+                        projectionsManager.DisableAsync(projectionName, new UserCredentials("admin", "changeit")).Wait();
+                    if (key.Key == ConsoleKey.E)
+                        projectionsManager.EnableAsync(projectionName, new UserCredentials("admin", "changeit")).Wait();
                     if (key.Key == ConsoleKey.A)
-                    {
-                        PublishTestEvent(conn);
-                    }
-                } while (key.Key == ConsoleKey.A);
+                        SetStreamMetadata(conn, streamName, new Dictionary<string, int> { { "$maxAge", 2 } });
+                    if (key.Key == ConsoleKey.C)
+                        SetStreamMetadata(conn, streamName, new Dictionary<string, int> { { "$maxCount", 2 } });
+                } while (true);
             }
             catch (Exception e)
             {
@@ -34,27 +59,52 @@ namespace ConsoleApp3
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
-       
+
         private static void Conn_Reconnecting(object sender, ClientReconnectingEventArgs e)
         {
             Console.WriteLine("Conn_Reconnecting...");
         }
 
-        private static void PublishTestEvent(IEventStoreConnection conn)
+        private static void SetStreamMetadata(IEventStoreConnection connection, string streamName, Dictionary<string, int> metadata)
         {
-            conn.AppendToStreamAsync("ciccio", ExpectedVersion.Any,
-                new EventData(Guid.NewGuid(), "CiccioCreated2", true, Encoding.UTF8.GetBytes("ciao ciao"), null)).Wait();
-            Console.WriteLine("Event published");
+            connection.SetStreamMetadataAsync(streamName, ExpectedVersion.Any, SerializeObject(metadata)).Wait();
+        }
+
+        static async Task AppendToStreamAsync(IEventStoreConnection cn, string streamName, int howManyEvents)
+        {
+            for (int i = 0; i < howManyEvents; i++)
+            {
+                await cn.AppendToStreamAsync(streamName, ExpectedVersion.Any,
+                    new EventData(Guid.NewGuid(), "test", false, new byte[] { 1, 2, 3 }, null));
+            }
+        }
+
+        private static byte[] SerializeObject(object obj)
+        {
+            var jsonObj = JsonConvert.SerializeObject(obj);
+            var data = Encoding.UTF8.GetBytes(jsonObj);
+            return data;
         }
 
         private static ConnectionSettings GetConnectionBuilder()
         {
             var settings = ConnectionSettings.Create().SetQueueTimeoutTo(TimeSpan.FromSeconds(1));
-                //.FailOnNoServerResponse();
-                //.LimitRetriesForOperationTo(1);
-                //.KeepRetrying()
-                //.KeepReconnecting();
             return settings;
         }
+    }
+
+    public class TestLogger : ILogger
+    {
+        public void Error(string format, params object[] args) { }
+
+        public void Error(Exception ex, string format, params object[] args) { }
+
+        public void Info(string format, params object[] args) { }
+
+        public void Info(Exception ex, string format, params object[] args) { }
+
+        public void Debug(string format, params object[] args) { }
+
+        public void Debug(Exception ex, string format, params object[] args) { }
     }
 }
