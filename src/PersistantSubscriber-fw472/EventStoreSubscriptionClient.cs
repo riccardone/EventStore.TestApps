51211589fd138e96bf80cbc2c05374edfa98b9f8
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-12-11 11:01:24 +0000

    new proj for persistantsubscription 4.7.2

---
+++ src/PersistantSubscriber-fw472/EventStoreSubscriptionClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.SystemData;
+
+namespace PersistantSubscriber_fw472
+{
+    public class EventStoreSubscriptionClient
+    {
+        private const string GroupName = "domain";
+        private const string StreamName = "$ce-domain";
+
+        private readonly IEventStoreConnection _eventStoreConnection;
+        private readonly UserCredentials _userCredentials;
+
+        private EventStorePersistentSubscriptionBase EventStorePersistentSubscriptionBase { get; set; }
+
+        public EventStoreSubscriptionClient(IEventStoreConnection eventStoreConnection, UserCredentials userCredentials)
+        {
+            _eventStoreConnection = eventStoreConnection;
+            _userCredentials = userCredentials;
+        }
+
+        public void Connect()
+        {
+            _eventStoreConnection.ConnectAsync().Wait();
+            EventStorePersistentSubscriptionBase = _eventStoreConnection.ConnectToPersistentSubscription(StreamName,
+                GroupName, EventAppeared, SubscriptionDropped, _userCredentials, 10, false);
+        }
+
+        private void SubscriptionDropped(EventStorePersistentSubscriptionBase subscription, SubscriptionDropReason reason, Exception ex)
+        {
+            Connect();
+        }
+
+        private static Task EventAppeared(EventStorePersistentSubscriptionBase subscription, ResolvedEvent resolvedEvent)
+        {
+            Console.WriteLine("Event appeared: " + resolvedEvent.Event.EventId);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/PersistantSubscriber-fw472/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.SystemData;
+
+namespace PersistantSubscriber_fw472
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriber = new EventStoreSubscriptionClient(
+                EventStoreConnection.Create(ConnectionSettings.Default, new Uri("http://localhost:1113"), "test"),
+                new UserCredentials("admin", "changeit"));
+            subscriber.Connect();
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
